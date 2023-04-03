ee5067b13ecb535119d900e449315de20213d38d
QiZi <531387937@qq.com>
2023-04-03 17:00:39 +0800

    change

---
+++ DX3D12/DX3D12/Chapter4/InitDirect3DApp.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "d3dApp.h"
+
+class InitDirect3DApp:public D3DApp
+{
+public:
+    InitDirect3DApp(HINSTANCE hInstance);
+    ~InitDirect3DApp();
+
+    bool Initialize() override;
+private:
+    void OnResize() override;
+    void Draw(const GameTimer& gt) override;
+    void Update(const GameTimer& gt) override;
+};
---
+++ DX3D12/DX3D12/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "Chapter4/InitDirect3DApp.h"
+#include "DirectXColors.h"
+
+using namespace DirectX;
+
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE preInstance,PSTR cmdLine,int showCmd)
+{
+#if defined(DEBUG)|defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    try
+    {
+        InitDirect3DApp theApp(hInstance);
+        if(!theApp.Initialize())
+            return 0;
+        
+        return theApp.Run();
+    }
+    catch (DxException &e)
+    {
+        MessageBox(nullptr,e.ToString().c_str(),L"HR Failed",MB_OK);
+    }
+}
