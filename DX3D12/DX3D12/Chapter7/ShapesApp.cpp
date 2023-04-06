a24c42a1ae901d7706034e108482940f0f78da45
QiZi <531387937@qq.com>
2023-04-06 10:49:36 +0800

    saved

---
+++ DX3D12/DX3D12/Chapter7/ShapesApp.cpp
@@ -0,0 +1,87 @@
+﻿#include "ShapesApp.h"
+
+ShapesApp::ShapesApp(HINSTANCE hInstance):D3DApp(hInstance)
+{
+}
+ShapesApp::~ShapesApp()
+{
+}
+
+bool ShapesApp::Initialize()
+{
+    if(!D3DApp::Initialize())
+    {
+        return false;
+    }
+    BuildConstantsBuffers();
+    BuildRootSignature();
+    BuildDescriptorHeaps();
+}
+
+void ShapesApp::OnResize()
+{
+    D3DApp::OnResize();
+}
+
+void ShapesApp::Update(const GameTimer& gt)
+{
+    mCurrFrameResourceIndex = (mCurrFrameResourceIndex+1)%gNumFrameResources;
+    mCurrFrameResource = mFrameResources[mCurrFrameResourceIndex].get();
+
+    if(mCurrFrameResource->Fence!=0&&mFence->GetCompletedValue()<mCurrFrameResource->Fence)
+    {
+        HANDLE eventHandle = CreateEventEx(nullptr,false,false,EVENT_ALL_ACCESS);
+        ThrowIfFailed(mFence->SetEventOnCompletion(mCurrFrameResource->Fence,eventHandle));
+        WaitForSingleObject(eventHandle,INFINITE);
+        CloseHandle(eventHandle);
+    }
+    
+}
+
+
+
+void ShapesApp::BuildFrameResources()
+{
+    for(int i = 0;i<gNumFrameResources;i++)
+    {
+        mFrameResources.push_back(std::make_unique<FrameResource>(md3dDevice.Get(),1,(UINT)mAllRitems.size()));
+    }
+}
+
+void ShapesApp::UpdateObjectCBs(const GameTimer& gt)
+{
+    auto currObjectCB = mCurrFrameResource->ObjectCB.get();
+    for(auto& e:mAllRitems)
+    {
+        if(e->NumFramesDirty>0)
+        {
+            XMMATRIX world = XMLoadFloat4x4(&e->World);
+
+            ObjectConstants objConstants;
+            XMStoreFloat4x4(&objConstants.Word,XMMatrixTranspose(world));
+
+            currObjectCB->CopyData(e->ObjCBIndex,objConstants);
+            e->NumFramesDirty--;
+        }
+    }
+}
+
+void ShapesApp::UpdateMainPassCB(const GameTimer& gt)
+{
+    XMMATRIX view = XMLoadFloat4x4(&mView);
+    XMMATRIX proj = XMLoadFloat4x4(&mProj);
+
+    XMMATRIX viewProj = XMMatrixMultiply(view,proj);
+    XMMATRIX invView = XMMatrixInverse(&XMMatrixDeterminant(view),view);
+    XMMATRIX invProj = XMMatrixInverse(&XMMatrixDeterminant(proj),proj);
+    XMMATRIX invViewProj = XMMatrixInverse(&XMMatrixDeterminant(viewProj),viewProj);
+
+    XMStoreFloat4x4(&mMainPassCB.View,XMMatrixTranspose(view));
+    XMStoreFloat4x4(&mMainPassCB.Proj,XMMatrixTranspose(proj));
+    XMStoreFloat4x4(&mMainPassCB.InvView,XMMatrixTranspose(invView));
+    XMStoreFloat4x4(&mMainPassCB.InvProj,XMMatrixTranspose(invProj));
+    XMStoreFloat4x4(&mMainPassCB.InvViewProj,XMMatrixTranspose(invViewProj));
+    mMainPassCB.EyePosW = mEyePos;
+}
+
+
---
+++ DX3D12/DX3D12/Chapter7/ShapesApp.h
@@ -0,0 +1,130 @@
+﻿#pragma once
+#include "d3dApp.h"
+#include "d3dUtil.h"
+#include "UploadBuffer.h"
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+struct ObjectConstants
+{
+    XMFLOAT4X4 Word = MathHelper::Identity4x4();
+};
+
+struct PassConstants
+{
+    XMFLOAT4X4 View;
+    XMFLOAT4X4 InvView;
+    XMFLOAT4X4 Proj;
+    XMFLOAT4X4 InvProj;
+    XMFLOAT4X4 ViewProj;
+    XMFLOAT4X4 InvViewProj;
+    XMFLOAT3 EyePosW;
+    XMFLOAT2 RenderTargetSize;
+    XMFLOAT2 InvRenderTargetSize;
+    float NearZ;
+    float FarZ;
+    float TotalTime;
+    float DeltaTime;
+};
+
+struct RenderItem
+{
+    RenderItem() = default;
+
+    XMFLOAT4X4 World = MathHelper::Identity4x4();
+    //dirty flag
+    int NumFramesDirty = gNumFrameResources;
+    //the Index in ConstantBuffer
+    UINT ObjCBIndex = -1;
+    //MeshGeoMetry to use
+    MeshGeometry* Geo = nullptr;
+
+    D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+    UINT IndexCount = 0;
+    UINT StartIndexLocation = 0;
+    int BaseVertexLocation = 0;
+};
+
+struct FrameResource
+{
+public:
+    FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount);
+    FrameResource(const FrameResource& rhs) = delete;
+    FrameResource& operator=(const FrameResource& rhs) = delete;
+    ~FrameResource();
+
+    ComPtr<ID3D12CommandAllocator> CmdListAlloc;
+
+    std::unique_ptr<UploadBuffer<PassConstants>> PassCB = nullptr;
+    std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = nullptr;
+
+    UINT64 Fence = 0;
+};
+
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount)
+{
+    ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,IID_PPV_ARGS(&CmdListAlloc)));
+    PassCB = std::make_unique<UploadBuffer<PassConstants>>(device,passCount,true);
+    ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device,objectCount,true);
+}
+
+inline FrameResource::~FrameResource()
+{
+}
+
+const int gNumFrameResources = 3;
+std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+FrameResource* mCurrFrameResource = nullptr;
+int mCurrFrameResourceIndex = 0;
+
+class ShapesApp:public D3DApp
+{
+public:
+    ShapesApp(HINSTANCE hInstance);
+    ShapesApp(const ShapesApp& rhs) = delete;
+    ShapesApp& operator=(const ShapesApp& rhs) = delete;
+    ~ShapesApp();
+    
+    bool Initialize() override;
+    void OnResize() override;
+    
+private:
+    void OnMouseDown(WPARAM btnState, int x, int y) override;
+    void OnMouseMove(WPARAM btnState, int x, int y) override;
+    void OnMouseUp(WPARAM btnState, int x, int y) override;
+    void Update(const GameTimer& gt) override;
+    void Draw(const GameTimer& gt) override;
+
+    void BuildFrameResources();
+    void BuildRootSignature();
+    void BuildConstantsBuffers();
+    void BuildDescriptorHeaps();
+    void BuildShaderAndInputLayout();
+    void BuildPSO();
+    void UpdateObjectCBs(const GameTimer& gt);
+    void UpdateMainPassCB(const GameTimer& gt);
+
+private:
+    ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
+    ComPtr<ID3D12DescriptorHeap> mCbvHeap = nullptr;
+    ComPtr<ID3DBlob> mvsByteCode = nullptr;
+    ComPtr<ID3DBlob> mpsByteCode = nullptr;
+    ComPtr<ID3D12PipelineState> mPSO = nullptr;
+
+    std::vector<std::unique_ptr<RenderItem>> mAllRitems;
+
+    std::vector<RenderItem*> mOpaqueRitems;
+    std::vector<RenderItem*> mTransparentRitems;
+
+    float mTheta = 1.5f*XM_PI;
+    float mPhi = XM_PIDIV4;
+    float mRadius = 5.0f;
+
+    XMFLOAT4X4 mView = MathHelper::Identity4x4();
+    XMFLOAT4X4 mProj = MathHelper::Identity4x4();
+
+    PassConstants mMainPassCB;
+
+    POINT mLastMousePos;
+};
