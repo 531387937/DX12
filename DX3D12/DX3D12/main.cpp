aade54d73a8819b4203dc2087a66adeeaaf5a8a8
QiZi <531387937@qq.com>
2023-06-01 17:50:54 +0800

    模板类再研究研究吧

--- DX3D12/DX3D12/main.cpp
+++ DX3D12/DX3D12/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include "DirectXColors.h"
-#include "Chapter9/CrateApp.h"
+#include "Chapter11/StencilApp.h"
 
 using namespace DirectX;
 
@@ -11,7 +11,7 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE preInstance,PSTR cmdLine,int sh
 
     try
     {
-        CrateApp theApp(hInstance);
+        StencilApp theApp(hInstance);
         if(!theApp.Initialize())
             return 0;
         
